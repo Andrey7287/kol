b854d724df984618ec57b6bc68113a7356bb1b5f
Андрей С. Алексеев <a.alekseev@INFODESIGN.LOCAL>
2016-12-19 09:25:11 +0300

    last

--- js/project.js
+++ js/project.js
@@ -1,14 +1,28 @@
-define(['jquery', 'slick', 'slimscroll', 'styler'], function(){
+define(['jquery', 'slick'], function(){
 
-  $.fn.ravno = function () {
-    var maxH = -1;
-    var $cols = $(this).height("auto").each(function () {
-      var h = $(this).height();
-      maxH = (h > maxH) ? h : maxH;
-    });
-    $cols.height(maxH);
-  };
+	$.fn.ravno = function () {
+		var maxH = -1;
+		var $cols = $(this).height("auto").each(function () {
+			var h = $(this).height();
+			maxH = (h > maxH) ? h : maxH;
+		});
+		$cols.height(maxH);
+	};
 
+	$('.slider').slick({
+		arrows: false,
+		dots: true,
+		autoplay: true
+	});
+
+	var $navLinks = $('.include');
+	$('.menu').on('click', '.include', function(e){
+		e.preventDefault();
+		$navLinks.removeClass('act');
+		$(this).addClass('act');
+		$(this).parent().siblings().find('ul').hide('slow');
+		$(this).next().toggle('slow');
+	});
 
 });
 
